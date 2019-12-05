65f2d91ef9250ef0a655b0a5d7e33d059cbbb23e
Aeva PaleceK <aeva.ntsc@gmail.com>
2019-12-04 23:59:43 -0600

    Alternate Implementation of New Ray Marching Approach
    This version just needs a small stack to track its progress, which
    should make it more shader friendly than before.

--- scratch.py
+++ scratch.py
@@ -2,6 +2,9 @@
 from math import *
 
 
+dont_care = 0.001
+
+
 def dot(lhs, rhs):
     acc = 0
     for a, b in zip(lhs, rhs):
@@ -52,6 +55,14 @@ def translate(point, offset):
 
 def sdf_union(*sdfs):
     return min(*sdfs)
+
+
+def sign(n):
+    return -1 if n < 0 else 1
+
+
+def pretty_float(ok_float):
+    return floor(abs(ok_float) * 10000000) / 10000000.0 * sign(ok_float)
 
 
 class sample:
@@ -121,6 +132,79 @@ def search(ray_start, ray_end, ray_dir, sdfn):
     return "NEW", samples, hit
 
 
+class coverage:
+    def __init__(self, tx, sd):
+        global dont_care
+        if sd is None:
+            self.low = tx
+            self.high = tx
+            self.sign = 0
+        else:
+            self.low = tx - abs(sd)
+            self.high = tx + abs(sd)
+            self.sign = -1 if sd < dont_care else 1
+
+    def __repr__(self):
+        return "({} -> {}: {})".format(self.low, self.high, self.sign)
+
+
+def search2(ray_start, ray_end, ray_dir, sdfn):
+    global dont_care
+    t_start = 0
+    t_end = distance(ray_end, ray_start)
+    t_mid = t_end * 0.5
+
+    pos_at_t = lambda t: add(ray_start, mul(ray_dir, t))
+    sd_at_t = lambda t: sdfn(pos_at_t(t))
+
+    stack = [coverage(t_end + dont_care, None), coverage(t_mid, sd_at_t(t_mid))]
+    target = coverage(0.0, None)
+
+    max_stack = len(stack)
+    iterations = 0
+    sdfn_calls = 1
+
+    while len(stack):
+        iterations += 1
+        if (stack[-1].low - dont_care) <= target.high:
+            target = stack.pop()
+            #print("pop: {}".format(target))
+            if target.sign < 0:
+                break
+        else:
+            t_next = (stack[-1].low + target.high) * 0.5
+            sd_next = sd_at_t(t_next)
+            sdfn_calls += 1
+            fnord = coverage(t_next, sd_next)
+            if stack[-1].sign == fnord.sign and (stack[-1].low - dont_care) <= fnord.high:
+                stack[-1].low = fnord.low
+                #print("merged: {}".format(stack[-1]))
+            else:
+                stack.append(coverage(t_next, sd_next))
+                #print("push: {}".format(stack[-1]))
+                max_stack = max(max_stack, len(stack))
+
+    cmp = search(ray_start, ray_end, ray_dir, sdfn)[-1]
+    if cmp is not None:
+        print("Ref Hit: {} -> {}".format(pos_at_t(cmp.tx), pretty_float(cmp.sd)))
+    else:
+        print("Ref Miss")
+
+    if target.sign == -1:
+        t_hit = max(t_start, target.low)
+        p_hit = pos_at_t(t_hit)
+        sd_hit = sdfn(p_hit)
+        print("Ray Hit: {} -> {}".format(p_hit, pretty_float(sd_hit)))
+    else:
+        print("Ray Miss")
+    print("Max Stack: {}".format(max_stack))
+    print("Iterations: {}".format(iterations))
+    print("SDF Calls: {}".format(sdfn_calls))
+
+    print("\n\n")
+    
+
+
 def sphere_march(ray_start, ray_end, ray_dir, sdfn):
     t_start = 0
     t_end = distance(ray_end, ray_start)
@@ -161,9 +245,7 @@ def report(run, trial, ray_start, ray_dir):
         print("\n  ray hit:")
         print("    {}".format(hit))
         dont_care = 0.001
-        #error = abs(hit.sd) / dont_care
-        #error = floor(error * 10000000) / 10000000.0
-        error = floor(abs(hit.sd) * 10000000) / 10000000.0
+        error = pretty_float(abs(hit.sd))
         print("    error: {}".format(error))
 
     else:
@@ -182,6 +264,14 @@ def case1():
     rprt(search(ray_start, ray_end, ray_dir, sdfn))
 
 
+def case1a():
+    ray_start = (-1.0, -2.0, 0.0)
+    ray_end = (1.0, 2.0, 0.0)
+    ray_dir = normalize(sub(ray_end, ray_start))
+    sdfn = lambda p: sdf_sphere(p, 1.0)
+    search2(ray_start, ray_end, ray_dir, sdfn)
+
+
 def case2():
     ray_start = (1.0, -2.0, 0.0)
     ray_end = (1.0, 2.0, 0.0)
@@ -192,6 +282,14 @@ def case2():
     rprt(search(ray_start, ray_end, ray_dir, sdfn))
 
 
+def case2a():
+    ray_start = (1.0, -2.0, 0.0)
+    ray_end = (1.0, 2.0, 0.0)
+    ray_dir = normalize(sub(ray_end, ray_start))
+    sdfn = lambda p: sdf_sphere(p, 1.0)
+    search2(ray_start, ray_end, ray_dir, sdfn)
+
+
 def case3():
     ray_start = (1.0, -2.0, 0.0)
     ray_end = (1.0, 2.0, 0.0)
@@ -200,6 +298,14 @@ def case3():
     rprt = lambda t: report("case 3", t, ray_start, ray_dir)
     rprt(sphere_march(ray_start, ray_end, ray_dir, sdfn))
     rprt(search(ray_start, ray_end, ray_dir, sdfn))
+
+
+def case3a():
+    ray_start = (1.0, -2.0, 0.0)
+    ray_end = (1.0, 2.0, 0.0)
+    ray_dir = normalize(sub(ray_end, ray_start))
+    sdfn = lambda p: sdf_sphere(p, 0.999)
+    search2(ray_start, ray_end, ray_dir, sdfn)
 
 
 def case4():
@@ -218,6 +324,18 @@ def case4():
     rprt(search(ray_start, ray_end, ray_dir, sdfn))
 
 
+def case4a():
+    ray_start = (0.0, -2.0, 0.0)
+    ray_end = (0.0, 2.0, 0.0)
+    ray_dir = normalize(sub(ray_end, ray_start))
+    def sdfn(point):
+        return sdf_union(
+            sdf_sphere(translate(point, (-1.0, 1.0, 0.0)), 0.999),
+            sdf_sphere(translate(point, (1.0, -1.0, 0.0)), 0.999),
+            sdf_sphere(translate(point, (-1.0, 1.0, 0.0)), 0.999))
+    search2(ray_start, ray_end, ray_dir, sdfn)
+
+
 def case5():
     ray_start = (0.0, -2.0, 0.0)
     ray_end = (0.0, 2.0, 0.0)
@@ -226,6 +344,14 @@ def case5():
     rprt = lambda t: report("case 5", t, ray_start, ray_dir)
     rprt(sphere_march(ray_start, ray_end, ray_dir, sdfn))
     rprt(search(ray_start, ray_end, ray_dir, sdfn))
+
+
+def case5a():
+    ray_start = (0.0, -2.0, 0.0)
+    ray_end = (0.0, 2.0, 0.0)
+    ray_dir = normalize(sub(ray_end, ray_start))
+    sdfn = lambda p: sdf_box(p, (1.0, 1.0, 1.0))
+    search2(ray_start, ray_end, ray_dir, sdfn)
 
 
 def case6():
@@ -241,6 +367,15 @@ def case6():
     rprt(search(ray_start, ray_end, ray_dir, sdfn))
 
 
+def case6a():
+    ray_start = (0.0, -2.0, 0.0)
+    ray_end = (0.0, 2.0, 0.0)
+    ray_dir = normalize(sub(ray_end, ray_start))
+    def sdfn(point):
+        return sdf_box(translate(point, (1.0, 0.0, 0.0)), (0.999, 2.0, 1.0))
+    search2(ray_start, ray_end, ray_dir, sdfn)
+
+
 def case7():
     ray_start = (0.0, -2.0, 0.0)
     ray_end = (0.0, 2.0, 0.0)
@@ -254,11 +389,29 @@ def case7():
     rprt(search(ray_start, ray_end, ray_dir, sdfn))
 
 
+def case7a():
+    ray_start = (0.0, -2.0, 0.0)
+    ray_end = (0.0, 2.0, 0.0)
+    ray_dir = normalize(sub(ray_end, ray_start))
+    def sdfn(point):
+        return sdf_box(translate(point, (0.0, -1.9, 0.0)), (1.0, 0.01, 1.0))
+    search2(ray_start, ray_end, ray_dir, sdfn)
+
+
 if __name__ == "__main__":
-    case1()
-    case2()
-    case3()
-    case4()
-    case5()
-    case6()
-    case7()
+    if 0:
+        case1()
+        case2()
+        case3()
+        case4()
+        case5()
+        case6()
+        case7()
+    else:
+        case1a()
+        case2a()
+        case3a()
+        case4a()
+        case5a()
+        case6a()
+        case7a()
