2a650045e11e94bb20ad71efd8882bd470f91ecb
Aeva PaleceK <aeva.ntsc@gmail.com>
2020-01-15 22:52:40 -0600

    Added Cube Bounds Elimination for Gloom Pass Ray Marching

--- shaders/raymarch.glsl
+++ shaders/raymarch.glsl
@@ -181,24 +181,36 @@ bool RayMarch(ObjectInfo Object, RayData Ray, out vec3 Position)
 
 float SoftRayMarch(ObjectInfo Object, const RayData Ray)
 {
-	const vec3 LocalRayDir = Ray.LocalDir;
-	const float MaxTravel = length(Ray.LocalStart) + length(Object.ShapeParams.xyz);
-	float Nearest = 1.0/0.0;
-	float Travel = 0.1;
-
-	for (int Step = 0; Step <= MaxIterations; ++Step)
+#if ENABLE_CUBETRACE
+	float Distance = CubeTrace(Object.ShapeParams.xyz, Ray);
+	if (Distance >= 0.0)
+#endif // ENABLE_CUBETRACE
 	{
-		const float SDF = SceneHull(Object.ShapeParams, Ray.LocalDir * Travel + Ray.LocalStart);
-		Nearest = min(SDF, Nearest);
-		if (IS_SOLID(SDF) || Travel >= MaxTravel)
-        {
-			break;
-        }
-		else
-		{
-			Travel += SDF;
-		}
-	}
+		const vec3 LocalRayDir = Ray.LocalDir;
+		const float MaxTravel = length(Ray.LocalStart) + length(Object.ShapeParams.xyz);
+		float Nearest = 1.0/0.0;
+		float Travel = 0.1;
 
-	return max(Nearest - AlmostZero, 0.0);
+		for (int Step = 0; Step <= MaxIterations; ++Step)
+		{
+			const float SDF = SceneHull(Object.ShapeParams, Ray.LocalDir * Travel + Ray.LocalStart);
+			Nearest = min(SDF, Nearest);
+			if (IS_SOLID(SDF) || Travel >= MaxTravel)
+			{
+				break;
+			}
+			else
+			{
+				Travel += SDF;
+			}
+		}
+
+		return max(Nearest - AlmostZero, 0.0);
+	}
+#if ENABLE_CUBETRACE
+	else
+	{
+		return 1.0;
+	}
+#endif // ENABLE_CUBETRACE
 }
