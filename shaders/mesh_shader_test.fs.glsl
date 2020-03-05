84ca4f378613da6457639813cf49d9b38dab1fb0
Aeva PaleceK <aeva.ntsc@gmail.com>
2020-03-05 00:40:12 -0600

    Slightly More Fancy Mesh Shader Test
    Now the mesh shader fills the screen with 32 tiles, each with a different
    color!

--- shaders/mesh_shader_test.fs.glsl
+++ shaders/mesh_shader_test.fs.glsl
@@ -3,7 +3,9 @@
 layout(location = 0) out vec4 OutColor;
 
 
+in flat vec2 Fnord;
+
 void main()
 {
-    OutColor = vec4(1.0, 0.0, 1.0, 1.0);
+    OutColor = vec4(Fnord.x, 0.0, Fnord.y, 1.0);
 }
--- shaders/mesh_shader_test.ms.glsl
+++ shaders/mesh_shader_test.ms.glsl
@@ -1,15 +1,27 @@
 --------------------------------------------------------------------------------
 
-layout(local_size_x=1) in;
-layout(max_vertices=4, max_primitives=2) out;
+#define THREADS 32
+#define VERTS 4
+#define PRIMS 2
+#define INDICES PRIMS * 3
+#define MAX_VERTS THREADS * VERTS
+#define MAX_PRIMS THREADS * PRIMS
+
+layout(local_size_x=THREADS) in;
+layout(max_vertices=MAX_VERTS, max_primitives=MAX_PRIMS) out;
 layout(triangles) out;
 
 out gl_MeshPerVertexNV {
     vec4 gl_Position;
-} gl_MeshVerticesNV[];
+} gl_MeshVerticesNV[]; // MAX_VERTS
 
 out uint gl_PrimitiveCountNV;
-out uint gl_PrimitiveIndicesNV[];
+out uint gl_PrimitiveIndicesNV[]; // MAX_PRIMS * 3
+
+out Interpolant {
+    vec2 Fnord;
+} Fnords[]; // MAX_VERTS
+
 
 // If we don't redeclare gl_PerVertex, compilation fails with the following error:
 // error C7592: ARB_separate_shader_objects requires built-in block gl_PerVertex to be redeclared before accessing its members
@@ -20,16 +32,32 @@ out gl_PerVertex {
 
 void main()
 {
-    //gl_LocalInvocationID.x
-    gl_MeshVerticesNV[0].gl_Position = vec4(-1.0, -1.0, 0.0, 1.0); // Upper Left
-    gl_MeshVerticesNV[1].gl_Position = vec4( 1.0, -1.0, 0.0, 1.0); // Upper Right
-    gl_MeshVerticesNV[2].gl_Position = vec4(-1.0,  1.0, 0.0, 1.0); // Bottom Left
-    gl_MeshVerticesNV[3].gl_Position = vec4( 1.0,  1.0, 0.0, 1.0); // Bottom Right
-    gl_PrimitiveIndicesNV[0] = 0;
-    gl_PrimitiveIndicesNV[1] = 1;
-    gl_PrimitiveIndicesNV[2] = 2;
-    gl_PrimitiveIndicesNV[3] = 2;
-    gl_PrimitiveIndicesNV[4] = 1;
-    gl_PrimitiveIndicesNV[5] = 3;
-    gl_PrimitiveCountNV += 2;
+    const float TileX = float(gl_LocalInvocationIndex % 8);
+    const float TileY = float(gl_LocalInvocationIndex / 8);
+    const vec2 TileSize = 2.0 / vec2(8.0, 4.0);
+    const float X1 = TileX * TileSize.x - 1.0;
+    const float Y1 = TileY * TileSize.y - 1.0;
+    const float X2 = X1 + TileSize.x;
+    const float Y2 = Y1 + TileSize.y;
+    const vec2 Fnord = vec2(TileX / 8.0, TileY / 4.0);
+    const uint TriangleOffset = gl_LocalInvocationIndex * VERTS;
+    const uint IndexOffset = gl_LocalInvocationIndex * INDICES;
+    gl_MeshVerticesNV[TriangleOffset + 0].gl_Position = vec4(X1, Y1, 0.0, 1.0); // Upper Left
+    gl_MeshVerticesNV[TriangleOffset + 1].gl_Position = vec4(X2, Y1, 0.0, 1.0); // Upper Right
+    gl_MeshVerticesNV[TriangleOffset + 2].gl_Position = vec4(X1, Y2, 0.0, 1.0); // Bottom Left
+    gl_MeshVerticesNV[TriangleOffset + 3].gl_Position = vec4(X2, Y2, 0.0, 1.0); // Bottom Right
+    Fnords[TriangleOffset + 0].Fnord = Fnord;
+    Fnords[TriangleOffset + 1].Fnord = Fnord;
+    Fnords[TriangleOffset + 2].Fnord = Fnord;
+    Fnords[TriangleOffset + 3].Fnord = Fnord;
+    gl_PrimitiveIndicesNV[IndexOffset + 0] = TriangleOffset + 0;
+    gl_PrimitiveIndicesNV[IndexOffset + 1] = TriangleOffset + 1;
+    gl_PrimitiveIndicesNV[IndexOffset + 2] = TriangleOffset + 2;
+    gl_PrimitiveIndicesNV[IndexOffset + 3] = TriangleOffset + 2;
+    gl_PrimitiveIndicesNV[IndexOffset + 4] = TriangleOffset + 1;
+    gl_PrimitiveIndicesNV[IndexOffset + 5] = TriangleOffset + 3;
+    if (gl_LocalInvocationIndex == 0)
+    {
+      gl_PrimitiveCountNV = MAX_PRIMS;
+    }
 }
