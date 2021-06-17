ce6d88ded6e5392582d9f13d9c4cc73d5f22c78d
Aeva Palecek <aeva.ntsc@gmail.com>
2021-06-17 02:34:00 -0500

    better adaptive tessellation

--- shaders/tessellatron/fine.tcs.glsl
+++ shaders/tessellatron/fine.tcs.glsl
@@ -27,17 +27,38 @@ void main()
 	gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;
 	tcs_out[gl_InvocationID].Normal = tcs_in[gl_InvocationID].Normal;
 	tcs_out[gl_InvocationID].CutShape = tcs_in[gl_InvocationID].CutShape;
-
+	if (tcs_in[0].CutShape > -1)
+	{
+		tcs_out[gl_InvocationID].Scratch.x = SceneFn(gl_in[gl_InvocationID].gl_Position.xyz);
+	}
+	else
+	{
+		tcs_out[gl_InvocationID].Scratch.x = Sphere(gl_in[gl_InvocationID].gl_Position.xyz, 2);
+	}
 	vec3 EyeRay = normalize(CameraOrigin.xyz - gl_in[gl_InvocationID].gl_Position.xyz);
-	tcs_out[gl_InvocationID].Scratch.x = dot(EyeRay, tcs_in[gl_InvocationID].Normal);
+	tcs_out[gl_InvocationID].Scratch.y = dot(EyeRay, tcs_in[gl_InvocationID].Normal);
 	barrier();
+
+	float Threshold = distance(
+		gl_in[(gl_InvocationID + 1) % 3].gl_Position.xyz,
+		gl_in[(gl_InvocationID + 2) % 3].gl_Position.xyz) / 4.0;
+	bool A = tcs_out[(gl_InvocationID + 1) % 3].Scratch.x < Threshold;
+	bool B = tcs_out[(gl_InvocationID + 2) % 3].Scratch.x < Threshold;
+
+	float Rate = (A || B) ? 7.0 : 1.0;
+
+	{
+		float Angle = min(
+			tcs_out[(gl_InvocationID + 1) % 3].Scratch.y,
+			tcs_out[(gl_InvocationID + 2) % 3].Scratch.y);
+		float Alpha = clamp(Angle, 0.0, 0.25) * 4.0;
+		Rate = mix(4.0, Rate, Alpha);
+	}
+
+	gl_TessLevelOuter[gl_InvocationID] = Rate;
 
 	if (gl_InvocationID == 0)
 	{
-		float Rate = 7.0;
-		gl_TessLevelInner[0] = Rate;
-		gl_TessLevelOuter[0] = Rate;
-		gl_TessLevelOuter[1] = Rate;
-		gl_TessLevelOuter[2] = Rate;
+		gl_TessLevelInner[0] = max(max(gl_TessLevelOuter[0], gl_TessLevelOuter[1]), gl_TessLevelOuter[2]);
 	}
 }
