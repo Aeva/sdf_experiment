cb6f09d4e98383694acc7ae4a12616d6c0ce3936
Aeva Palecek <aeva.ntsc@gmail.com>
2021-06-20 22:38:39 -0500

    fixed adaptive tessellation

--- shaders/tessellatron/fine.tcs.glsl
+++ shaders/tessellatron/fine.tcs.glsl
@@ -30,11 +30,11 @@ void main()
 	bool CutShape = IsCutShape(tcs_in[0].ShapeID);
 	if (CutShape)
 	{
-		tcs_out[gl_InvocationID].Scratch.x = SceneFn(gl_in[gl_InvocationID].gl_Position.xyz, tcs_in[0].ShapeID);
+		tcs_out[gl_InvocationID].Scratch.x = SceneWholeFn(gl_in[gl_InvocationID].gl_Position.xyz);
 	}
 	else
 	{
-		tcs_out[gl_InvocationID].Scratch.x = Sphere(gl_in[gl_InvocationID].gl_Position.xyz, tcs_in[0].ShapeID);
+		tcs_out[gl_InvocationID].Scratch.x = Sphere(gl_in[gl_InvocationID].gl_Position.xyz, 2);
 	}
 	vec3 EyeRay = normalize(CameraOrigin.xyz - gl_in[gl_InvocationID].gl_Position.xyz);
 	tcs_out[gl_InvocationID].Scratch.y = dot(EyeRay, tcs_in[gl_InvocationID].Normal);
